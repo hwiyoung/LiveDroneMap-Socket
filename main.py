818c2e0f7399490722ef845d28c1f468f461dcb5
hwiyoung <huddy1204@gmail.com>
2020-09-14 17:12:02 +0900

    feat: Add functions to rectify images

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import sys
 import socket
 import selectors
 import types
-from socket_module import receive
+from socket_module import receive, send
 import json
 import numpy as np
 import drones
 import georef_for_eo as georeferencers
+import rectifiers
+import time
 
 sel_server = selectors.DefaultSelector()
 sel_client = selectors.DefaultSelector()
@@ -43,21 +45,13 @@ def service_connection(key_s, mask_s, key_c, mask_c):
     if mask_s & selectors.EVENT_READ:
         try:
             taskID, frameID, latitude, longitude, altitude, roll, pitch, yaw, camera, img = receive(sock_s)
-            # print(taskID, frameID, latitude, longitude, altitude, roll, pitch, yaw, camera)
             if taskID is None:
                 return
 
+            start_time = time.time()
             # 1. Set IO
             my_drone = drones.Drones(make=camera, pre_calibrated=False)
             # my_drone = drones.Drones(make=camera, ground_height=38.0, pre_calibrated=True)  # Only for test - Jeonju
-            sensor_width = my_drone.sensor_width
-            focal_length = my_drone.focal_length
-            gsd = my_drone.gsd
-            ground_height = my_drone.ground_height
-            R_CB = my_drone.R_CB
-            comb = my_drone.comb
-            manufacturer = my_drone.manufacturer
-            pre_calibrated = my_drone.pre_calibrated
 
             # 2. System calibration & CCS converting
             init_eo = np.array([longitude, latitude, altitude, roll, pitch, yaw])
@@ -68,41 +62,20 @@ def service_connection(key_s, mask_s, key_c, mask_c):
                 my_georeferencer = georeferencers.DirectGeoreferencer()
                 adjusted_eo = my_georeferencer.georeference(my_drone, init_eo)
 
-            print("Check")
+            # 3. Rectify
+            my_rectifier = rectifiers.AverageOrthoplaneRectifier(height=my_drone.ground_height)
+            bbox_wkt, orthophoto = my_rectifier.rectify(img, my_drone, adjusted_eo)
 
-            # # 3. Rectify
-            # my_rectifier = rectifiers.AverageOrthoplaneRectifier(height=my_drone.ground_height)
-            # bbox_wkt, orthophoto = my_rectifier.rectify(img, my_drone, adjusted_eo)
-            #
-            #         logging.info('========================================================================================')
-            #         logging.info('========================================================================================')
-            #         logging.info('A new image is received.')
-            #         logging.info('File name: %s' % frameID)
-            #         logging.info('Current Drone: %s' % my_drone.__class__.__name__)
-            #         logging.info('========================================================================================')
-            #
-            #         send(frameID, taskID, frameID, 0, bbox_wkt, [], orthophoto, client)    # 메타데이터 생성/ send to client
-            #         print(time.time() - start_time)
-        ########################
+            send(frameID, taskID, frameID, 0, bbox_wkt, [], orthophoto, sock_c)  # 메타데이터 생성/ send to client
+            print("Elapsed time:", format(time.time() - start_time, ".2f"))
         except Exception as e:
             print(e)
             print("closing connection to", data_s.addr)
-            sel_server.unregister(sock_s)
-            sock_s.close()
-        ########################
-    # if mask & selectors.EVENT_WRITE:
-    #     if not data.outb:
-    #         data.outb = []
-    #     if data.outb:
-    #         print('sending', repr(data.outb), 'to connection', data.connid)
-    #         try:
-    #             sent = sock.send(data.outb)  # Should be ready to write
-    #             data.outb = data.outb[sent:]
-    #         except Exception as e:
-    #             print("send in service_connection:", e)
-    #             sel.unregister(sock)
-    #             sock.close()
-    #             return
+            # sel_server.unregister(sock_s)
+            # sock_s.close()
+            sel_client.close()
+            # sel_client.unregister(sock_c)
+            # sock_c.close()
 
 
 with open("config.json") as f:
@@ -131,18 +104,18 @@ try:
     while True:
         events_servers = sel_server.select(timeout=None)
         events_clients = sel_client.select(timeout=None)
-        # for key_server, mask_server in events_servers:
-        for events_servers, events_clients in zip(events_servers, events_clients):
-            key_server = events_servers[0]
-            mask_server = events_servers[1]
-            key_client = events_clients[0]
-            mask_client = events_clients[1]
+        for events_server, events_client in zip(events_servers, events_clients):
+            key_server = events_server[0]
+            mask_server = events_server[1]
+            key_client = events_client[0]
+            mask_client = events_client[1]
             if key_server.data is None:
                 accept_wrapper(key_server.fileobj)
             else:
                 service_connection(key_server, mask_server, key_client, mask_client)
         # Check for a socket being monitored to continue
-        if not sel_server.get_map():
+        if not sel_client.get_map():
+            sel_client = selectors.DefaultSelector()
             start_connections(CLIENT_IP, CLIENT_PORT, num_conns=num_conn)
 except KeyboardInterrupt:
     print("caught keyboard interrupt, exiting")
